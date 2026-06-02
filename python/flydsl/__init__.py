d240d4082ef22eb30b0094ca3a0321f4f7393fd1
Felix Li <felix.li@amd.com>
2026-06-02 16:41:16 +0800

    Bump FlyDSL version to 0.2.0 (#609)

--- python/flydsl/__init__.py
+++ python/flydsl/__init__.py
@@ -2,6 +2,6 @@
 # Copyright (c) 2025 FlyDSL Project Contributors
 # ruff: noqa: I001
 
-__version__ = "0.1.9"
+__version__ = "0.2.0"
 
 from .autotune import Config as Config, autotune as autotune  # noqa: E402
