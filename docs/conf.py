b57dee50b23938106f045ef78e843a7e17e97bd3
Felix Li <felix.li@amd.com>
2026-06-16 21:47:20 +0800

    [Chore] Bump version to 0.2.2 (#697)

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, os.path.join(_project_root, "python"))
 project = "FlyDSL"
 copyright = "2024-2026, Advanced Micro Devices, Inc."
 author = "AMD"
-release = "0.2.1"
+release = "0.2.2"
 
 # -- General configuration ---------------------------------------------------
 extensions = [
